125d690181984848a6b8a16f74c4f0f3004c9ce2
vzvu3k6k <vzvu3k6k@gmail.com>
2018-08-16 02:55:30 +0900

    Define unary operator (negative operator)

--- grammar.js
+++ grammar.js
@@ -2,7 +2,10 @@ module.exports = grammar({
   name: 'simple_arithmetic',
 
   rules: {
-    source_file: $ => choice($.number),
+    source_file: $ => $.node,
+    node: $ => choice($.unary, $.number),
+    unary: $ => seq($.negative_operator, $.node),
+    negative_operator: $ => '-',
     number: $ => /\d+/
   }
 });
